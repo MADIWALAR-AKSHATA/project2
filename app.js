dd99ed072a44225b5d84006981bbd6c9d5825fad
Akshata <akshatamadiwalar123@gmail.com>
2024-02-17 17:46:43 +0530

    came to 1 step back.

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
 // add new feature - button
 // add new feature - form
-
+//mistake
 let p = document.getElementsByTagName("p")[1];
 let h1 = document.getElementsByTagName("h1")[1];
 p.innerText = "para Tag updated by diff DOM";
